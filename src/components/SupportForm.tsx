4b2713e71c9ed882d222eba0329cc5b96f71c97d
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-05-30 01:59:31 -0300

    getYear
    objeto para capturar ano atual

--- src/components/SupportForm.tsx
+++ src/components/SupportForm.tsx
@@ -28,6 +28,7 @@ const SupportForm = () => {
         const currentDate = new Date();
         const currentDay = currentDate.getDate().toString();
         const currentMonth = (currentDate.getMonth() + 1).toString();
+        const currentYear = currentDate.getFullYear().toString();
     }, [])
 
 
