7fc678299219068f340da2abe7e667645803c483
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-05-30 04:24:37 -0300

    Footer
     parte inferior da página, garantindo que ele permaneça fixado no final da tela

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="bg-blue-500 p-4 mt-auto">
+            <div className="max-w-7xl mx-auto flex justify-between items-center">
+                <div className="text-white text-lg">Siga-nos nas redes sociais:</div>
+                <div className="flex items-center">
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
