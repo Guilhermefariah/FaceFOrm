f5d860d90c0378d176ca58e1f2e06e175ed3b026
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-05-30 02:05:09 -0300

    setFormData
    atualizando o estado de forma imutavel

--- src/components/SupportForm.tsx
+++ src/components/SupportForm.tsx
@@ -29,6 +29,10 @@ const SupportForm = () => {
         const currentDay = currentDate.getDate().toString();
         const currentMonth = (currentDate.getMonth() + 1).toString();
         const currentYear = currentDate.getFullYear().toString();
+
+        setFormData(prevState => ({
+
+        }))
     }, [])
 
 
