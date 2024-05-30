83b1f1d459aa7fdccca0cfab14568a88d13aee05
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-05-30 04:18:40 -0300

    Header
    componente cria uma barra de navegação simples com o nome do aplicativo à esquerda e links para contas de mídia social à direita.

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+const Navbar = () => {
+    return (
+        <nav className="bg-blue-500 p-4">
+            <div className="max-w-7xl mx-auto flex justify-between items-center">
+                <div className="text-white text-3xl font-bold">FaceForm</div>
+                <div className="flex items-center ml-4">
+                    <a href="https://github.com" className="text-white mx-2" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faGithub} size="2x" />
+                    </a>
+                    <a href="https://linkedin.com" className="text-white mx-2" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                    </a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
