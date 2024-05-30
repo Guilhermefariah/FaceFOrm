12f4a78b8dac7ccde9d852cfbc4681a8ec1db3fd
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-05-30 01:17:36 -0300

    interface
    garantir que os dados chegam ao componente

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,4 +3,12 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from "axios";
 
-
+interface formData {
+    firsName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    day: string;
+    month: string;
+    year: string;
+}
