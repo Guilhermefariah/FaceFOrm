2e9ba052b1a3f423f44817aa27b0292aaa868e5e
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-05-30 02:56:59 -0300

    required
    Yup required ano

--- src/components/SupportForm.tsx
+++ src/components/SupportForm.tsx
@@ -45,16 +45,16 @@ const SupportForm = () => {
 
     const daysArray = Array.from({
         length: dayToMonth(Number(formData.month),
-            Number(formData.year))
-    }, (_, index) => index + 1);
+        Number(formData.year))}, (_, index) => index + 1)
 
     const valideSchema = Yup.object({
-        firstName: Yup.string().required("Nome é obrigatorio"),
-        lastName: Yup.string().required("Sobre nome é obrigatorio"),
-        email: Yup.string().email("Email inválido").required("Email é obrigatorio"),
-        password: Yup.string().min(6, "A senha deve ter pelo menos 6 caracteres").required("Senha é obrigatoria"),
-        day: Yup.string().required("Dia é obrigatorio"),
-        month: Yup.string().required("Mês é obrigatorio"),
+        firstName: Yup.string().required('Nome é obrigatório'),
+        lastName: Yup.string().required('Sobrenome é obrigatório'),
+        email: Yup.string().email('Email inválido').required('Email é obrigatório'),
+        password: Yup.string().min(6, 'A senha deve ter pelo menos 6 caracteres').required('Senha é obrigatória'),
+        day: Yup.string().required('Dia é obrigatório'),
+        month: Yup.string().required('Mês é obrigatório'),
+        year: Yup.string().required('Ano é obrigatório')
     })
 
 
