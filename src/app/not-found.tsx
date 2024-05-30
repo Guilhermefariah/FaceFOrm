df98fa245f26769bf5de15491d94e698f6a0ff5e
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-05-30 14:57:42 -0300

    not-found
    style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
-export default function NontFound(){
+export default function NotFound() {
     return (
-        <>
-            <h1>404</h1>
-            <p>Página não exite</p>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-red-500">404</h1>
+            <p className="text-lg text-gray-700">Página não existe</p>
             <Link href="/" passHref>
-                <div>Voltar</div>
+                <div className="mt-4 text-blue-500 hover:underline cursor-pointer">Voltar</div>
             </Link>
-        </>
-    )
+        </div>
+    );
 }
